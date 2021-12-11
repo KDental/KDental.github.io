37139e44fb7a66dc1b837e4de6cd1e33e02f4eba
KDental <92442669+KDental@users.noreply.github.com>
2021-12-11 19:37:04 +0000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// Side menu Function
+
+function menuFunction() {
+  document.getElementById("sideMenuContents").style.right = "0px";
+}
+function closedBtnFunction() {
+  document.getElementById("sideMenuContents").style.right = "-100%";
+}
